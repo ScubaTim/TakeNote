02c35e65bc3e3763ed5a05d761d11ec6041f447a
Tim Gentry <gentry.tim@gmail.com>
2020-09-11 22:44:40 -0400

    frontend connected to backend and recieving data

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,8 @@
 const express = require('express');
-const { response } = require('express');
 const app = express();
+const cors = require('cors')
 
+app.use(cors())
 app.use(express.json());
 
 let notes = [
--- takenote/src/components/App.js
+++ takenote/src/components/App.js
@@ -1,37 +1,29 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import Toolbar from './Toolbar';
 import NotesList from './NotesList';
 import NoteEditor from './NoteEditor';
 import { Container } from 'reactstrap';
 
-let notes = [
-    {
-        id: 1,
-        content: "HTML is easy",
-        date: "2019-05-30T17:30:31.098Z",
-        important: true
-    },
-    {
-        id: 2,
-        content: "Browser can execute only Javascript",
-        date: "2019-05-30T18:39:34.091Z",
-        important: false
-    },
-    {
-        id: 3,
-        content: "GET and POST are the most important methods of HTTP protocol",
-        date: "2019-05-30T19:20:14.298Z",
-        important: true
-    }
-];
-
 const App = () => {
+    const [notes, setNotes] = useState([]);
     const [newNote, setNewNote] = useState(false);
 
     const handleNewNote = () => {
         setNewNote(!newNote);
     }
+    console.log()
 
+    useEffect(() => {
+        axios
+            .get('http://localhost:3001/notes')
+            .then(response => {
+                const notes = response.data
+                setNotes(notes);
+            })
+    }, []);
+
+    //If no notes to display, show note editor instead.
     if (notes.length === 0) {
         return (
             <div>
@@ -43,6 +35,7 @@ const App = () => {
         );
     }
 
+    //If there are notes, show NoteList unless newNote is pressed, then show NoteEditor.
     return (
         <div>
             <Toolbar createNote={handleNewNote} />
